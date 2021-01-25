27808b85f5366f8fd1019432af47bde5af232d7f
Kai <kai5266@gmail.com>
2021-01-25 17:25:17 +0800

    initial commit

---
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.guess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.guess.R.layout.activity_main)
+        R.drawable.pig
+    }
+}
