7f39de5e9bedc006e29ec36ee1c927d4978e38bd
Kai <kai5266@gmail.com>
2021-01-29 00:47:44 +0800

    finish ViewModel, LiveData

--- app/src/main/java/com/example/guess/GuessViewModel.kt
+++ app/src/main/java/com/example/guess/GuessViewModel.kt
@@ -2,17 +2,39 @@ package com.example.guess
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.*
 
 class GuessViewModel : ViewModel() {
+    var secret = 0
+    var count = 0
     val counter = MutableLiveData<Int>()
+    val result = MutableLiveData<GameResult>()
 
     init {
-        counter.value = 0
+        counter.value = count
+        reset() //初始化時即呼叫reset(),以產生secret
     }
 
     fun guess(num: Int) {
-        var n = counter.value ?: 0
-        n++
-        counter.value = n
+        if (num == -111) result.value = GameResult.ENTER_NUMBER
+        else {
+            result.value = when (num - secret) {
+                0 -> GameResult.RIGHT
+                in 1..Int.MAX_VALUE -> GameResult.SMALLER
+                else -> GameResult.BIGGER
+            }
+            count++
+            counter.value = count
+        }
     }
+
+    fun reset() {
+        secret = Random().nextInt(10) + 1
+        count = 0
+        counter.value = count
+    }
+}
+
+enum class GameResult {
+    BIGGER, SMALLER, RIGHT, ENTER_NUMBER
 }
--- app/src/main/java/com/example/guess/MaterialActivity.kt
+++ app/src/main/java/com/example/guess/MaterialActivity.kt
@@ -17,7 +17,6 @@ class MaterialActivity : AppCompatActivity() {
     private lateinit var viewModel: GuessViewModel
     lateinit var binding: ActivityMaterialBinding
     lateinit var contentBinding: ContentMaterialBinding
-    val secretNumber = SecretNumber()
     val TAG = MaterialActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,23 +24,40 @@ class MaterialActivity : AppCompatActivity() {
         binding = ActivityMaterialBinding.inflate(layoutInflater)
         contentBinding = binding.contentMaterial
         setContentView(binding.root)
-        Log.d(TAG, "secret: ${secretNumber.secret}")
         setSupportActionBar(binding.toolbar)
         viewModel = ViewModelProvider(this).get(GuessViewModel::class.java)
         viewModel.counter.observe(this, Observer {
             contentBinding.counter.text = it.toString()
         })
+        viewModel.result.observe(this, Observer {
+            val message = when (it) {
+                GameResult.BIGGER -> getString(R.string.bigger)
+                GameResult.SMALLER -> getString(R.string.smaller)
+                GameResult.RIGHT -> getString(R.string.you_got_it)
+                GameResult.ENTER_NUMBER -> getString(R.string.enter_number)
+            }
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.dialog_title))
+                .setMessage(message)
+                .setPositiveButton(getString(R.string.ok)) { _, _ ->
+                    contentBinding.edNumber.text.clear()
+                }
+                .show()
 
-
+        })
 
         binding.fab.setOnClickListener { view ->
             replay()
         }
-
     }
 
     fun check(view: View) {
-        viewModel.guess(3)
+        val number = if (contentBinding.edNumber.text.isNotEmpty()) {
+            contentBinding.edNumber.text.toString().toInt()
+        } else {
+            -111
+        }
+        viewModel.guess(number)
 //        val number = contentBinding.edNumber.text.toString().toInt()
 //        contentBinding.counter.text = secretNumber.count.toString()
 //        Log.d(TAG, "number: $number")
@@ -62,13 +78,12 @@ class MaterialActivity : AppCompatActivity() {
 //            .show()
     }
 
-    fun replay(){
+    private fun replay() {
         AlertDialog.Builder(this)
             .setTitle("Replay Game")
             .setMessage("Are you sure?")
             .setPositiveButton(getString(R.string.ok)) { dialog, which ->
-                secretNumber.reset()
-                contentBinding.counter.text = secretNumber.count.toString()
+                viewModel.reset()
                 contentBinding.edNumber.text.clear()
             }
             .setNeutralButton("Cancel", null)
